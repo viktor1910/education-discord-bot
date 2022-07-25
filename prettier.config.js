4f381c34577d5c744a40a4ed21f71b6b1b83b7ad
viktor <nguyenvu19.dev@gmail.com>
2022-07-25 15:45:37 +0700

    feat: eslint config

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	printWidth: 80,
+	tabWidth: 2,
+	useTabs: true,
+	semi: true,
+	singleQuote: true,
+	trailingComma: 'es5',
+	bracketSpacing: true,
+	jsxBracketSameLine: false,
+	arrowParens: 'avoid',
+	endOfLine: 'auto',
+	endOfLine: 'crlf',
+};
